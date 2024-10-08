9a818b5ef6ed91ff9fb830c448fd760a5b312cb6
jmcglock <85720969+jmcglock@users.noreply.github.com>
2024-10-08 10:01:34 -0400

    .

--- equinix/raid0-lvm.sh
+++ equinix/raid0-lvm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # RAID devices
 declare -a DEVICES=(
   "/dev/nvme1n1"
@@ -10,21 +9,16 @@ declare -a DEVICES=(
   "/dev/nvme2n1"
   "/dev/nvme3n1"  
 )
-
-# Custom RAID device name
-RAID_DEVICE_NAME="md69"
-
-echo "Stopping any existing RAID $RAID_DEVICE_NAME"
-mdadm --stop /dev/$RAID_DEVICE_NAME
-
-echo "Creating RAID 0 array $RAID_DEVICE_NAME"
-mdadm --create /dev/$RAID_DEVICE_NAME --level=0 --raid-devices=${#DEVICES[@]} "${DEVICES[@]}"
-
+# RAID device name
+RAID_DEVICE_ID="md127"
+echo "Stopping any existing RAID $RAID_DEVICE_ID"
+mdadm --stop /dev/$RAID_DEVICE_ID
+echo "Creating RAID 0 array $RAID_DEVICE_ID"
+mdadm --create /dev/$RAID_DEVICE_ID --level=0 --raid-devices=${#DEVICES[@]} "${DEVICES[@]}"
 # Wait for RAID to become active 
-while ! cat /proc/mdstat | grep -q "$RAID_DEVICE_NAME"; do
+while ! cat /proc/mdstat | grep -q "$RAID_DEVICE_ID"; do
   sleep 1
 done
-
 echo "Saving RAID config to mdadm.conf" 
 > /etc/mdadm.conf
 mdadm --detail --scan >> /etc/mdadm.conf
